1f136727b889cd55f09dfc27b8e1159173466e28
xnought <bertuccd@oregonstate.edu>
2022-10-30 20:41:26 -0700

    feat: read in from standard input

--- main.c
+++ main.c
@@ -1,7 +1,38 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_LINE 256
+
+void readStdIn(char *buffer)
+{
+	fgets(buffer, MAX_LINE, stdin);
+
+	/* it records the new line, so remove that */
+	size_t lineLength = strlen(buffer);
+	if (lineLength > 0)
+	{
+		buffer[lineLength - 1] = 0;
+	}
+}
+
+/*
+	Print a : and get the user input
+	returns the size of the buffer
+ */
+void commandPrompt(char *lineBuffer)
+{
+	/* command prompts with ':' and expects an input */
+	printf(": ");
+	readStdIn(lineBuffer);
+}
 
 int main()
 {
-	printf("%s", "Working...");
+	char *buffer[MAX_LINE];
+	while (1)
+	{
+		commandPrompt(buffer);
+		printf("%s\n", buffer);
+	}
 }
