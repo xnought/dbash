bcea5ea2f3d3fef218077e01186465fa5e5edd0d
xnought <bertuccd@oregonstate.edu>
2022-11-03 21:05:18 -0700

    feat: parse the buffer for useful information

--- smallsh2.c
+++ smallsh2.c
@@ -14,9 +14,9 @@ struct cmd
 
 	/*
 		all the arguments parsed from the tokens
-		: script args_1...args_512
+		: cmdName args_1...args_512
 	*/
-	char *script;
+	char *cmdName;
 	char *args[MAX_ARGS];
 	int argCount;
 
@@ -36,7 +36,7 @@ void resetCmd(struct cmd *cmdInfo)
 	clearArrayOfStrings(cmdInfo->args, MAX_ARGS);
 	cmdInfo->argCount = 0;
 
-	cmdInfo->script = NULL;
+	cmdInfo->cmdName = NULL;
 
 	cmdInfo->inputRedir = NULL;
 	cmdInfo->outputRedir = NULL;
@@ -72,9 +72,85 @@ void readIntoBuffer(char *buffer)
 	}
 }
 
-void parseCmdPrompt(char *buffer, struct cmd *cmdInfo)
+void addArg(struct cmd *cmdInfo, char *token)
 {
+	cmdInfo->args[cmdInfo->argCount] = token;
+	cmdInfo->argCount++;
+}
+void addToken(struct cmd *cmdInfo, char *token)
+{
+	cmdInfo->tokens[cmdInfo->tokenCount] = token;
+	cmdInfo->tokenCount++;
+}
+
+void parseCmdPrompt(char *buffer, struct cmd *cmdInfo, int foregroundMode)
+{
+	char *rest = buffer;
+	char *token;
+	int hitSpecialChar = -1;
+	char *lastWord;
+
 	resetCmd(cmdInfo);
+
+	/* the first token is the command name */
+	token = strtok_r(rest, " ", &rest);
+	addToken(cmdInfo, token);
+	cmdInfo->cmdName = token;
+
+	/* the next tokens are arguments until we hit a '<' or a '>' */
+	while ((token = strtok_r(rest, " ", &rest)) != NULL && cmdInfo->tokenCount < MAX_TOKENS)
+	{
+		/* add each as a token */
+		addToken(cmdInfo, token);
+		if (hitSpecialChar == -1)
+		{
+
+			if (strcmp(token, "<") == 0 || strcmp(token, ">") == 0 || strcmp(token, "&") == 0)
+			{
+				/* subtract 1 since we want an index */
+				hitSpecialChar = cmdInfo->tokenCount - 1;
+			}
+			else
+			{
+				/* if not a special char, its an arg!*/
+				addArg(cmdInfo, token);
+			}
+		}
+	}
+
+	int i = hitSpecialChar;
+	while (i < cmdInfo->tokenCount)
+	{
+
+		if (strcmp(cmdInfo->tokens[i], "<") == 0)
+		{
+			i++;
+			cmdInfo->inputRedir = cmdInfo->tokens[i];
+		}
+		else if (strcmp(cmdInfo->tokens[i], ">") == 0)
+		{
+			i++;
+			cmdInfo->outputRedir = cmdInfo->tokens[i];
+		}
+		else if (strcmp(cmdInfo->tokens[i], "&") == 0 && foregroundMode == 0)
+		{
+			cmdInfo->background = 1;
+		}
+		i++;
+	}
+
+	/* if a background process, send io to dev/null if not specified */
+	if (cmdInfo->background)
+	{
+		if (cmdInfo->inputRedir == NULL)
+		{
+			cmdInfo->inputRedir = "dev/null";
+		}
+		if (cmdInfo->outputRedir == NULL)
+		{
+			cmdInfo->outputRedir = "dev/null";
+		}
+	}
 }
 
 void getCmdPrompt(char *buffer)
@@ -89,14 +165,15 @@ int main()
 {
 	char buffer[MAX_BUFFER];
 	struct cmd cmdInfo;
+	int foregroundMode = 0;
 
 	while (1)
 	{
 		/* 1. The command prompt */
 		getCmdPrompt(buffer);
 
-		/* 2. parse the buffer for the command */
-		parseCmdPrompt(buffer, &cmdInfo);
+		/* 2. parse the buffer for the command for key info */
+		parseCmdPrompt(buffer, &cmdInfo, foregroundMode);
 	}
 
 	return 0;
